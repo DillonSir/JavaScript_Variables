dba3be9e6d7be461c17814f3c958901a7a9e1f3e
DillonSir <dillonsirmans1@gmail.com>
2022-06-06 22:44:58 -0400

    Completed exercise

--- app.js
+++ app.js
@@ -3,8 +3,37 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+const firstName = "Dillon";
+let lastName = "Sirmans";
+var age = 23; 
 
-// Exercise 2
+lastName = "Sirmans";
+age = 23;
 
-// Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let inCaseSensitive = true;
+
+console.log(`${language} was first release in ${createdYear}.`)
+
+let product = "T-shirty";
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log (
+  `${product} in size ${selectedSize} is ${price} with ${stock} in stock`
+)
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log (
+  `${title} by ${author} is ${pageCount} page long. Your last bookmark was page ${bookmark}`
+  )
